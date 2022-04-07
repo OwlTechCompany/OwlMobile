9ac5052971762b3d53ab6b34b5dc5ad168d25f41
AnastasiaHolovash <nastyuha.holovasch@gmail.com>
2022-04-07 17:00:43 +0300

    Initial Commit

---
+++ Owl/OwlApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwlApp.swift
+//  Owl
+//
+//  Created by Anastasia Holovash on 07.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct OwlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
