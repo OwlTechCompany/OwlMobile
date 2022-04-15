302c83a28c183bbd848f94fc0be7efb9c47ff124
ddanilyuk <danis.danilyuk@gmail.com>
2022-04-15 12:36:10 +0300

    accelerate loader animation

--- Owl/Modules/Common/Loader.swift
+++ Owl/Modules/Common/Loader.swift
@@ -7,21 +7,14 @@
 
 import SwiftUI
 
-let darkBlack = Color(red: 17/255, green: 18/255, blue: 19/255)
-let darkGray = Color(red: 41/255, green: 42/255, blue: 48/255)
-let darkBlue = Color(red: 96/255, green: 174/255, blue: 201/255)
-let darkPink = Color(red: 244/255, green: 132/255, blue: 177/255)
-let darkViolet = Color(red: 214/255, green: 189/255, blue: 251/255)
-let darkGreen = Color(red: 137/255, green: 192/255, blue: 180/255)
-
-let clearWhite = Color(red: 17/255, green: 18/255, blue: 19/255)
-let clearGray = Color(red: 181/255, green: 182/255, blue: 183/255)
-let clearBlue = Color(red: 116/255, green: 166/255, blue: 240/255)
+let darkBlue = Color(red: 96 / 255, green: 174 / 255, blue: 201 / 255)
+let darkPink = Color(red: 244 / 255, green: 132 / 255, blue: 177 / 255)
+let darkViolet = Color(red: 214 / 255, green: 189 / 255, blue: 251 / 255)
 
 struct Loader: View {
 
-    let rotationTime: Double = 0.75
-    let animationTime: Double = 1.9 // Sum of all animation times
+    let rotationTime: Double = 0.5
+    let animationTime: Double = 1.3 // Sum of all animation times
     let fullRotation: Angle = .degrees(360)
     static let initialDegree: Angle = .degrees(270)
 
@@ -68,7 +61,7 @@ struct Loader: View {
         }
         .onAppear {
             animateSpinner()
-            Timer.scheduledTimer(withTimeInterval: animationTime, repeats: true) { mainTimer in
+            Timer.scheduledTimer(withTimeInterval: animationTime, repeats: true) { _ in
                 self.animateSpinner()
             }
         }
@@ -76,10 +69,10 @@ struct Loader: View {
 
     // MARK: Animation methods
     func animateSpinner(
-        with duration: Double,
+        with delay: Double,
         completion: @escaping (() -> Void)
     ) {
-        Timer.scheduledTimer(withTimeInterval: duration, repeats: false) { _ in
+        Timer.scheduledTimer(withTimeInterval: delay, repeats: false) { _ in
             withAnimation(Animation.easeInOut(duration: self.rotationTime)) {
                 completion()
             }
@@ -87,22 +80,26 @@ struct Loader: View {
     }
 
     func animateSpinner() {
-        animateSpinner(with: rotationTime) { self.spinnerEndS1 = 1.0 }
+        animateSpinner(with: 0) {
+            self.spinnerEndS1 = 1.0
+        }
 
-        animateSpinner(with: (rotationTime * 2) - 0.025) {
+        animateSpinner(with: rotationTime - 0.025) {
             self.rotationDegreeS1 += fullRotation
             self.spinnerEndS2S3 = 0.8
         }
 
-        animateSpinner(with: (rotationTime * 2)) {
+        animateSpinner(with: rotationTime) {
             self.spinnerEndS1 = 0.03
             self.spinnerEndS2S3 = 0.03
         }
 
-        animateSpinner(with: (rotationTime * 2) + 0.0525) { self.rotationDegreeS2 += fullRotation
+        animateSpinner(with: rotationTime + 0.0525) {
+            self.rotationDegreeS2 += fullRotation
         }
 
-        animateSpinner(with: (rotationTime * 2) + 0.225) { self.rotationDegreeS3 += fullRotation
+        animateSpinner(with: rotationTime + 0.225) {
+            self.rotationDegreeS3 += fullRotation
         }
     }
 
