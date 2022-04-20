720c57607f00063f7fb68cb38f804a0d9592e035
ddanilyuk <danis.danilyuk@gmail.com>
2022-04-20 20:23:21 +0300

    Add animationState

--- Owl/Modules/Main/Profile/ProfileView.swift
+++ Owl/Modules/Main/Profile/ProfileView.swift
@@ -112,271 +112,327 @@ private extension UIEdgeInsets {
     }
 }
 
+//enum ScrollState {
+//    case small
+//    case blur
+//    case photoHidden
+//
+//    var showBlur: Bool {
+//        return self == .photoHidden
+//    }
+//
+//    init(offsetY: CGFloat) {
+//        let max: CGFloat = 157 + 1
+//        switch offsetY {
+//        case (..<CGFloat(max / 2)):
+//            self = .small
+//        case (max / 2..<max):
+//            self = .blur
+//        case (max...):
+//            self = .photoHidden
+//            //            case (70...):
+//            //                self = .navigation
+//        default:
+//            print("Default: \(offsetY)")
+//            self = .small
+//        }
+//    }
+//}
+
+enum PhotoState {
+    case big
+    case small
+
+    var height: CGFloat {
+        switch self {
+        case .big:
+            return screen.width
+        case .small:
+            return 100
+        }
+    }
+
+    var width: CGFloat {
+        switch self {
+        case .big:
+            return screen.width
+        case .small:
+            return 100
+        }
+    }
+
+    var cornerRadius: CGFloat {
+        switch self {
+        case .big:
+            return 0
+        case .small:
+            return height / 2
+        }
+    }
+
+    var isScaled: Bool {
+        return self == .big
+    }
+
+    mutating func toggle() {
+        switch self {
+        case .big:
+            self = .small
+        case .small:
+            self = .big
+        }
+    }
+}
+
+struct AnimationState {
+
+    // MARK: - Constants
+
+    let textSize: CGFloat = 44
+    let subTextSize: CGFloat = 24
+    let smallPhotoTopPadding: CGFloat = 10
+    var textHeaderSize: CGFloat {
+        textSize + subTextSize
+    }
+
+    @Environment(\.safeAreaInsets) var safeAreaInsets
+
+    // MARK: - Variables
+
+    var offset: CGFloat = .zero
+    var photoState: PhotoState = .small
+
+    var smallPhotoMaxY: CGFloat {
+        return PhotoState.small.height + safeAreaInsets.top + smallPhotoTopPadding
+    }
+}
 
 
 struct ProfileView: View {
 
     var store: Store<Profile.State, Profile.Action>
 
-    @State var offset: CGFloat = .zero
-    @State var photoScaled: Bool = false
+    @State var animationState: AnimationState = .init()
+
+//    @State var offset: CGFloat = .zero
     @ObservedObject var delegate = Delegate()
 
-    @Environment(\.safeAreaInsets) private var safeAreaInsets
+//    @Environment(\.safeAreaInsets) private var safeAreaInsets
 
-    @State var scrollState: ScrollState = .init(offsetY: 0)
-    @State var photoState: PhotoState = .small
+//    @State var scrollState: ScrollState = .init(offsetY: 0)
+//    @State var photoState: PhotoState = .small
 
-    enum PhotoState {
-        case big
-        case small
+//    @State var textColor: Color = .black
 
-        var isScaled: Bool {
-            return self == .big
-        }
-
-//        var width: CGFloat {
-//            switch
-//        }
-
-        func imageOffset(scrollOffset: CGFloat) -> CGFloat {
-            switch self {
-            case .big:
-                return scrollOffset < 0 ? scrollOffset : 0
-            case .small:
-                return 0
-            }
-        }
-
-        func imageHeight(scrollOffset: CGFloat, smallInset: CGFloat = 0) -> CGFloat {
-            switch self {
-            case .big:
-                return screen.width - scrollOffset
-            case .small:
-                return 100 + smallInset
-            }
-        }
-
-        func imageWidth() -> CGFloat {
-            switch self {
-            case .big:
-                return screen.width
-            case .small:
-                return 100
-            }
-        }
-    }
-
-    enum ScrollState {
-        case small
-        case blur
-        case photoHidden
-
-        var showBlur: Bool {
-            return self == .photoHidden
-        }
-
-        func phoneOpacity(offset: CGFloat) -> CGFloat {
-            let max: CGFloat = 157 - 44
-            let result = (max - offset) / max
-//            print(result)
-            return result
-        }
-
-        func blurOpacity(offset: CGFloat) -> CGFloat {
-            let max: CGFloat = 157 + 1
-
-            switch offset {
-            case (...(max - 10)):
-                return 0
-            case ((max - 10)..<max + 10):
-                let result = (offset - max + 10) / (20)
-                print(result)
-                return result
-            default:
-                return 1
-            }
-//            let result = 1 - ((max - offset) / max)
-//            return result
-        }
-
-        init(offsetY: CGFloat) {
-            let max: CGFloat = 157 + 1
-            switch offsetY {
-            case (..<CGFloat(max / 2)):
-                self = .small
-            case (max / 2..<max):
-                self = .blur
-            case (max...):
-                self = .photoHidden
-//            case (70...):
-//                self = .navigation
-            default:
-                print("Default: \(offsetY)")
-                self = .small
-            }
-        }
-    }
-
-    var textHeaderSize: CGFloat {
-        textSize + subTextSize
-    }
-    let textSize: CGFloat = 44
-    let subTextSize: CGFloat = 24
-    let smallImageTopPadding: CGFloat = 10
-
-    var imageMaxY: CGFloat {
-        return PhotoState.small.imageHeight(scrollOffset: 0) + safeAreaInsets.top + smallImageTopPadding
-    }
-
-//    var image
-    // 147 + 30 = 100 heigth 47 safeArea 30 topPadding
 
     var body: some View {
-        WithViewStore(store) { viewStore in
+        WithViewStore(store) { _ in
             ScrollView {
                 VStack(spacing: 30) {
+                    ZStack {
+                        UserImageView(animationState: $animationState)
+                            .onTapGesture { animationState.photoState.toggle() }
 
-                    GeometryReader { proxy in
-                        ZStack {
-                            Image(uiImage: Asset.Images.nastya.image)
-                                .resizable()
-                                .scaledToFill()
-                                .cornerRadius(photoState.isScaled ? 0 : 50)
-                                .offset(x: 0, y: photoState.isScaled ? offset : 0)
-                                .frame(
-                                    width: photoState.imageWidth(),
-                                    height: photoState.imageHeight(scrollOffset: offset)
-                                )
-                                .padding(.top, photoState.isScaled ? 0 : safeAreaInsets.top + smallImageTopPadding)
-                                .padding(.bottom, photoState.isScaled ? 0 : textHeaderSize)
-//                                .ignoresSafeArea(.all, edges: .top)
+                        HeaderBlurView(animationState: $animationState)
 
-                            VStack {
-                                Color.clear.background(.ultraThinMaterial)
-                                    .opacity(scrollState.blurOpacity(offset: offset))
-                                    .frame(height: safeAreaInsets.top + textSize, alignment: .top)
-//                                    .animation(.default, value: scrollState.showBlur)
-                                    .offset(x: 0, y: offset)
-
-                                Spacer()
-                            }
-
-                            VStack(spacing: 0) {
-
-                                Spacer()
-
-                                Text("Anastasia Holovash")
-                                    .font(.system(.headline))
-                                    .foregroundColor(photoState.isScaled ? .white.opacity(0.9) : .black)
-                                    .frame(height: textSize)
-                                    .frame(width: screen.width)
-//                                    .background(Color.red.opacity(0.2))
-                                    .offset(x: 0, y: offset > (imageMaxY - textSize) ? offset - (imageMaxY - textSize) : 0)
-                                    .offset(x: 0, y: photoState.isScaled && offset < 0 ? offset : 0)
-
-                                Text("+380931314850")
-                                    .foregroundColor(photoState.isScaled ? .white.opacity(0.7) : .black.opacity(0.7))
-                                    .font(.system(.caption))
-                                    .frame(width: screen.width)
-                                    .frame(height: subTextSize)
-//                                    .background(Color.orange.opacity(0.2))
-                                    .offset(x: 0, y: photoState.isScaled && offset < 0 ? offset : 0)
-                                    .opacity(scrollState.phoneOpacity(offset: offset))
-                            }
-//                            .background(Color.blue)
-                        }
+                        HeaderDescriptionView(animationState: $animationState)
                     }
-
                     .frame(width: screen.width)
-                    .frame(
-                        height: photoState.imageHeight(
-                            scrollOffset: offset,
-                            smallInset: safeAreaInsets.top + smallImageTopPadding + textHeaderSize
-                        )
-                    )
-                    .onTapGesture {
-                        photoScaled.toggle()
-                        photoState = photoState == .small ? .big : .small
-                    }
-                    .zIndex(100)
+                    .zIndex(2)
 
                     VStack {
-                        ForEach(0..<100, id: \.self) { i in
+                        ForEach(0..<100, id: \.self) { index in
                             HStack {
                                 Spacer()
-                                Text("\(i)")
+                                Text("\(index)")
                                 Spacer()
                             }
                         }
                     }
-                    .zIndex(99)
+                    .zIndex(1)
                 }
             }
-            .onChange(of: photoState, perform: { newValue in
+            .onChange(of: animationState.photoState) { newValue in
                 switch newValue {
                 case .big:
-                    simpleSuccess()
+                    generateFeedback()
                 case .small:
                     break
                 }
-            })
+            }
             .onChange(of: delegate.scrollViewDidScroll) { value in
-                let newOffset = value!.scrollView.contentOffset.y
-                offset = newOffset
-                scrollState = ScrollState(offsetY: newOffset)
-                print("SCROLL STATE \(scrollState) \(newOffset)")
-                if newOffset < -10 {
-                    photoState = .big
-//                    photoScaled = true
-                } else if newOffset > 10 {
-                    photoState = .small
-//                    photoScaled = false
+                let offset = value!.scrollView.contentOffset.y
+                animationState.offset = offset
+                if offset < -10 {
+                    animationState.photoState = .big
+                } else if offset > 10 {
+                    animationState.photoState = .small
                 }
             }
             .onChange(of: delegate.scrollViewDidEndDragging) { value in
                 let scrollView = value!.scrollView
                 let offset = scrollView.contentOffset.y
-                if (0..<imageMaxY / 2).contains(offset) {
+                let hidePhotoOffset = animationState.smallPhotoMaxY
+                let edgePosition = hidePhotoOffset / 2
+
+                switch offset {
+                case (0..<edgePosition):
                     scrollView.setContentOffset(.zero, animated: true)
-                } else if (imageMaxY / 2..<imageMaxY).contains(offset) {
-                    scrollView.setContentOffset(.init(x: 0, y: imageMaxY), animated: true)
+
+                case (edgePosition..<hidePhotoOffset):
+                    scrollView.setContentOffset(
+                        CGPoint(x: 0, y: hidePhotoOffset),
+                        animated: true
+                    )
+
+                default:
+                    break
                 }
             }
-            .animation(.spring(response: 0.35, dampingFraction: 0.7, blendDuration: 0), value: photoState)
+            .animation(
+                Animation.spring(response: 0.35, dampingFraction: 0.7, blendDuration: 0),
+                value: animationState.photoState
+            )
             .introspectScrollView { scrollView in
                 if scrollView.delegate !== delegate {
-                    print("************** setup DELEGATE")
                     scrollView.delegate = delegate
                 }
             }
             .ignoresSafeArea()
         }
-
         .background(
-            Color(UIColor.systemGroupedBackground)
+            Color(.systemGroupedBackground)
                 .edgesIgnoringSafeArea(.all)
         )
         .navigationBarHidden(true)
     }
 
-    func simpleSuccess() {
-//        let generator = UINotificationFeedbackGenerator()
-//        generator.notificationOccurred(.warning)
+    func generateFeedback() {
         let impactHeavy = UIImpactFeedbackGenerator(style: .heavy)
         impactHeavy.impactOccurred()
+    }
+}
 
+struct UserImageView: View {
+
+    @Binding var animationState: AnimationState
+
+    var body: some View {
+        Image(uiImage: Asset.Images.nastya.image)
+            .resizable()
+            .scaledToFill()
+            .cornerRadius(animationState.photoState.cornerRadius)
+            .offset(x: 0, y: animationState.photoState.isScaled ? animationState.offset : 0)
+            .frame(
+                width: animationState.photoState.width,
+                height: animationState.imageViewHeight
+            )
+            .padding(.top, animationState.photoState.isScaled ? 0 : animationState.safeAreaInsets.top + animationState.smallPhotoTopPadding)
+            .padding(.bottom, animationState.photoState.isScaled ? 0 : animationState.textHeaderSize)
+    }
+}
+
+private extension AnimationState {
+
+    var imageViewHeight: CGFloat {
+        switch photoState {
+        case .big:
+            return photoState.height - offset
+        case .small:
+            return photoState.height
+        }
+    }
+}
+
+
+struct HeaderBlurView: View {
+
+    @Binding var animationState: AnimationState
+
+    var body: some View {
+        VStack {
+            Color.clear.background(.ultraThinMaterial)
+                .opacity(animationState.blurOpacity)
+                .frame(height: animationState.blurHeight, alignment: .top)
+                .offset(x: 0, y: animationState.offset)
+
+            Spacer()
+        }
+    }
+}
+
+private extension AnimationState {
+
+    var blurHeight: CGFloat {
+        safeAreaInsets.top + textSize
+    }
+
+    var blurOpacity: CGFloat {
+        let max: CGFloat = smallPhotoMaxY + 1
+
+        switch offset {
+        case (...(max - 10)):
+            return 0
+        case ((max - 10)..<max + 10):
+            return (offset - max + 10) / (20)
+        default:
+            return 1
+        }
+    }
+}
+
+
+struct HeaderDescriptionView: View {
+
+    @Binding var animationState: AnimationState
+
+    var body: some View {
+        VStack(spacing: 0) {
+
+            Spacer()
+
+            Text("Anastasia Holovash")
+                .font(.system(.headline))
+                .frame(height: animationState.textSize)
+                .frame(width: screen.width)
+                .offset(x: 0, y: animationState.imageOffset)
+                .offset(x: 0, y: animationState.photoState.isScaled && animationState.offset < 0 ? animationState.offset : 0)
+                .foregroundColor(animationState.mainColor)
+
+            Text("+380931314850")
+                .foregroundColor(animationState.photoState.isScaled ? .white.opacity(0.7) : .black.opacity(0.7))
+                .font(.system(.caption))
+                .frame(width: screen.width)
+                .frame(height: animationState.subTextSize)
+                .offset(x: 0, y: animationState.photoState.isScaled && animationState.offset < 0 ? animationState.offset : 0)
+                .opacity(animationState.phoneOpacity)
+        }
+    }
+}
+
+extension AnimationState {
+    var mainColor: Color {
+        photoState.isScaled ? .white.opacity(0.9) : .black
+    }
+
+    var phoneOpacity: CGFloat {
+        let max: CGFloat = smallPhotoMaxY - textSize
+        return (max - offset) / max
+    }
+
+    var imageOffset: CGFloat {
+        return offset > (smallPhotoMaxY - textSize) ? offset - (smallPhotoMaxY - textSize) : 0
     }
 }
 
 // MARK: - Preview
 
-struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileView(store: Store(
-            initialState: Profile.State(image: Asset.Images.owlWithPadding.image),
-            reducer: Profile.reducer,
-            environment: Profile.Environment()
-        ))
-    }
-}
+//struct ProfileView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ProfileView(store: Store(
+//            initialState: Profile.State(image: Asset.Images.owlWithPadding.image),
+//            reducer: Profile.reducer,
+//            environment: Profile.Environment()
+//        ))
+//    }
+//}
